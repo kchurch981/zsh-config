85915d1d14f0c5c5b67e6ac02f4f32fa1edfeb76
root <kchurch981@gmail.com>
2019-10-27 17:23:18 -0400

    Update

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ chsh -s $(which zsh)
 
 #Setup oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir ~/.config/terminator
 sed -i '11s/robbyrussell/agnoster/' ~/.zshrc
 
 #Setup terminator
