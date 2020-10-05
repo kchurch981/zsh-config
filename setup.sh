0f254a03bd4f8b36b348fb7aaa9f352c2828da1d
kchurch981 <55496294+kchurch981@users.noreply.github.com>
2020-10-05 15:59:45 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,16 +2,16 @@
 #Setup compile YCM
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --all
-
+cd -
 #Setup zsh
 apt-get install -y zsh
 chsh -s $(which zsh) 
 
 #Setup oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-mkdir ~/.config/terminator
 sed -i '11s/robbyrussell/agnoster/' ~/.zshrc
 
 #Setup terminator
+mkdir -p ~/.config/terminator
 apt-get install terminator
 cp ./config_terminator ~/.config/terminator/config
